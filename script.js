4dd82d5c3d4bc0636eb177e0afac2f0d08a40de0
StoicaO <125612945+StoicaO@users.noreply.github.com>
2023-05-25 00:01:39 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,74 +1,133 @@
-const circleRed = "🔴"
-const circcleYellow = "🙂"
-let turnPlayer = circleRed;
-let boxes = Array.from(document.getElementsByClassName("box"));
-let array = Array(42).fill(null);
+const boxRed = "Red";
+const boxYellow = "Yellow";
+let currPlayer = boxRed;
 
-for (let box of boxes) {
-    box.addEventListener('click', boxClicked);
+const rows = 6;
+const colum = 7;
+let blockCell = false;
+let board ;
+
+
+function createGrid() {
+    let start = document.getElementById("Start");
+    start.style.display = "none";
+    board = [];
+   
+    for(let i = 0; i < rows; ++i) {
+        let row = [];
+        for(let j = 0; j < colum; ++j){
+            row.push(" ");
+            let grid = document.createElement("div");
+            grid.id = i.toString() + "-" + j.toString();
+            grid.classList.add('grid');
+            grid.addEventListener("click", coloringCell);
+            document.getElementById("board").append(grid);
+        }
+        board.push(row);
+        
+    }
     
 }
 
-function boxClicked(e){
-    const id = e.target.id;
-    if(!array[id]){
-        array[id] = turnPlayer;
-        e.target.innerText = turnPlayer;
-        if(playerWon()){
-            messageWinner.innerHTML = `${turnPlayer} has won!🎉`;
-        }
+function coloringCell(){
+
+    if (blockCell) {
+        return;
     }
-    turnPlayer = turnPlayer === circleRed ? circcleYellow : circleRed;
+    let coord = this.id.split("-"); //"0 - 0" --> ["0", "0"]
+    console.log(coord);
+    let r = Number(coord[0]);
+    let c = Number(coord[1]);
+
+
+    board[r][c] = currPlayer;
+    let cell = document.getElementById(r.toString() + "-" + c.toString());
+    if (currPlayer == boxRed){
+        cell.classList.add("redPice");
+        currPlayer = boxYellow;
+    } else {
+        cell.classList.add("yellowPice");
+        currPlayer = boxRed;
+    }
+    
+
+    if (horizontally() ){
+        setWinner(r, c);
+    } else if (Vertically ()) {
+        setWinner(r, c);
+    } else if (secoDiagonal()) { 
+        setWinner(r, c);
+    } else if (mainDiagonal()) {
+        setWinner(r, c);
+    }
 }
 
-
-function playerWon() {
-    for(let i = 42; i >= 0; --i){
-        if(array[i] === turnPlayer){
-            if(array[i + 1] === turnPlayer && array[i + 2] === turnPlayer && array[i + 3] === turnPlayer) {
-                document.getElementById(i).style.backgroundColor = "red";
-                document.getElementById(i + 1).style.backgroundColor = "red";
-                document.getElementById(i + 2).style.backgroundColor = "red";
-                document.getElementById(i + 3).style.backgroundColor = "red";
-                disableTable();
-                return true;
-
-            }else if(array[i - 7] === turnPlayer && array[i - 14] === turnPlayer && array[i - 21] === turnPlayer) {
-                document.getElementById(i).style.backgroundColor = "red";
-                document.getElementById(i -7).style.backgroundColor = "red";
-                document.getElementById(i - 14).style.backgroundColor = "red";
-                document.getElementById(i - 21).style.backgroundColor = "red";
-                disableTable();
-                return true;
-            }else if(array[i - 6] === turnPlayer && array[i - 12] === turnPlayer && array[i - 18] === turnPlayer) {
-                document.getElementById(i).style.backgroundColor = "red";
-                document.getElementById(i - 6).style.backgroundColor = "red";
-                document.getElementById(i - 12).style.backgroundColor = "red";
-                document.getElementById(i - 18).style.backgroundColor = "red";
-                disableTable();
-                return true;
-            }else if(array[i - 8] === turnPlayer && array[i - 16] === turnPlayer && array[i - 24] === turnPlayer) {
-                document.getElementById(i).style.backgroundColor = "red";
-                document.getElementById(i - 8).style.backgroundColor = "red";
-                document.getElementById(i - 16).style.backgroundColor = "red";
-                document.getElementById(i - 24  ).style.backgroundColor = "red";
-                disableTable();
-                return true;
+function horizontally() {
+    for(let r = 0; r < rows; ++r) {
+        for(let c = 0; c < colum - 3; ++c) {
+            if (board[r][c]  != " ") {
+                if (board[r][c] == board[r][c + 1] && 
+                    board[r][c + 1] == board[r][c + 2] && 
+                    board[r][c + 2] == board[r][c + 3]) {
+                    return true;
+                } 
             }
-            
+        }
+    }
+}
 
+function Vertically () {
+    for(let c = 0; c < colum; ++c){
+        for(let r = 0; r < rows - 3; ++r) {
+            if (board[r][c] != " ") {
+                if (board[r][c] == board[r + 1][c] && 
+                    board[r + 1][c] == board[r + 2][c] && 
+                    board[r + 2][c] == board[r + 3][c]) {
+                    return true;
+                }
+            }  
+        }
+    }
+}
+
+function secoDiagonal()  {
+    for(let r = 3; r < rows ; ++r ){
+        for(let c = 0; c < colum - 3; ++c) {
+            if (board[r][c] != " "){
+                if (board[r][c] == board[r - 1][c + 1] &&
+                    board[r - 1][c + 1] == board[r - 2][c + 2] &&
+                    board[r - 2][c + 2] == board[r - 3][c + 3]) {
+                    return true;
+                }
+            }
+        }
+    }
+}
+function mainDiagonal () {
+    for (let r = 0; r < rows - 3; ++r) {
+        for (let c = 0; c < colum - 3; ++c) {
+            if (board[r][c] != " ") {
+                 if (board[r][c] == board[r + 1][c + 1] &&
+                    board[r + 1][c + 1] == board[r + 2][c + 2] &&
+                    board[r + 2][c + 2] == board[r + 3][c + 3]) {
+                    return true;
+                }
+            }
         }
     }
 
 }
 
-
-function disableTable() {
-    for (let box of boxes) {
-        box.removeEventListener('click', boxClicked);  
+function setWinner(r, c) {
+    let winner = document.getElementById("messageWinner");
+    if (board[r][c] == boxRed) {
+        winner.innerText = `${boxRed} has won!🎉`;
+    } else {
+        winner.innerText = `${boxYellow} has won!🎉`;
     }
+    blockCell = true;
 }
-
+   
 function resetGame() {
     location.reload();
 }
